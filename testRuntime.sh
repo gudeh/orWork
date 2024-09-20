90f835e826ccea0c385b6f002cab44ff0f155544
gudeh <augusto.berndt@precisioninno.com>
2024-09-20 13:53:22 +0000

    add script for measuring runtime of tests
    Signed-off-by: gudeh <augusto.berndt@precisioninno.com>

---
+++ testRuntime.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+NUM_RUNS=3
+
+> runtimeTest.log
+> fullOutput.log
+#cd test
+
+for i in $(seq 1 $NUM_RUNS); do
+  echo "Running iteration $i..." | tee -a runtimeTest.log fullOutput.log
+  
+  # { time openroad -no_init -threads 1 incremental02.tcl -exit; } 2>&1 | tee -a fullOutput.log | grep -E '^real|^user|^sys' >> runtimeTest.log
+    { time ./regression; } 2>&1 | tee -a fullOutput.log | grep -E '^real|^user|^sys' >> runtimeTest.log
+  
+done
