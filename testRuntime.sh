cd53fea18e32179081635022de9e14afd62faa85
Augusto Berndt <augusto.berndt@precisioninno.com>
2025-04-23 09:12:38 +0000

    include run-me on runtime measurements
    Signed-off-by: Augusto Berndt <augusto.berndt@precisioninno.com>

--- testRuntime.sh
+++ testRuntime.sh
@@ -10,6 +10,7 @@ for i in $(seq 1 $NUM_RUNS); do
   echo "Running iteration $i..." | tee -a runtimeTest.log fullOutput.log
   
   # { time openroad -no_init -threads 1 incremental02.tcl -exit; } 2>&1 | tee -a fullOutput.log | grep -E '^real|^user|^sys' >> runtimeTest.log
-    { time ./regression; } 2>&1 | tee -a fullOutput.log | grep -E '^real|^user|^sys' >> runtimeTest.log
+  # { time ./regression; } 2>&1 | tee -a fullOutput.log | grep -E '^real|^user|^sys' >> runtimeTest.log
+  { time ./run-me-swerv-nangate45-base.sh; } 2>&1 | tee -a fullOutput.log | grep -E '^real|^user|^sys' >> runtimeTest.log
   
 done
