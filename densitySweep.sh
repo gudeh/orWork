c054417bd590ae3f50f2b270292338a1f07ff2ae
Augusto Berndt <augusto.berndt@precisioninno.com>
2025-11-18 13:01:39 +0000

    verific density sweep for rpaidus
    Signed-off-by: Augusto Berndt <augusto.berndt@precisioninno.com>

--- densitySweep.sh
+++ densitySweep.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
 
-PLACE_DENSITY_VALUES=(0.15 0.20 0.201 0.22 0.24 0.25 0.26 0.28 0.30 0.35)
+PLACE_DENSITY_VALUES=(0.1 0.53 0.6 0.8)
 
-DESIGN_CONFIG="./designs/sky130hd/microwatt/config.mk"
+DESIGN_CONFIG="designs/rapidus2hp/hercules_is_int/config.mk"
 
 STRING_PARAM="$1"
 
 
 for PLACE_DENSITY in "${PLACE_DENSITY_VALUES[@]}"; do
     echo "Opening terminal for PLACE_DENSITY=${PLACE_DENSITY}"
-    gnome-terminal -- bash -c "make DESIGN_CONFIG='$DESIGN_CONFIG' FLOW_VARIANT='$PLACE_DENSITY' PLACE_DENSITY='$PLACE_DENSITY' $STRING_PARAM; exec bash" #
+
+    # Construct the command string clearly
+    CMD="make \
+    DESIGN_CONFIG='$DESIGN_CONFIG' \
+    FLOW_VARIANT='$PLACE_DENSITY' \
+    PLACE_DENSITY='$PLACE_DENSITY' \
+    SYNTH_HDL_FRONTEND=verific \
+    PLATFORM_HOME=/workspace/rapidus/current/rapidus/ \
+    $STRING_PARAM"
+
+    # Pass the variable to the terminal
+    gnome-terminal -- bash -c "$CMD; exec bash"
+    
     sleep 0.5
 done
 
